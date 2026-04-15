d7ff2f647aab48fa94a9ecd5876675c6525c91b2
psthejohn <psthejohn@gmail.com>
2026-04-15 21:48:51 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/scale_my_mails/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
